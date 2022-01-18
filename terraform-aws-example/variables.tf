cf92617dea79bc63679ab80932db4c501384d8e9
Juan Guinle <juan.guinle@gmail.com>
2022-01-18 12:43:33 -0300

    nuevos examples

---
+++ terraform-aws-example/variables.tf
@@ -0,0 +1,38 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ENVIRONMENT VARIABLES
+# Define these secrets as environment variables
+# ---------------------------------------------------------------------------------------------------------------------
+
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "instance_name" {
+  description = "The Name tag to set for the EC2 Instance."
+  type        = string
+  default     = "terratest-example"
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type to run."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "(Optional) A mapping of tags to assign to the resource."
+  default = {
+    Name  = "Flugel"
+    Owner = "Infrateam"
+  }
+}
