a70db1f91accfc681ae03f5709d657d324825a43
Juan Guinle <juan.guinle@gmail.com>
2022-01-24 13:22:51 -0300

    clean up 01

--- main_test.go
+++ main_test.go
@@ -28,13 +28,8 @@ func TestTerraformS3AndEC2Tags(t *testing.T) {
 	apply, err := terraform.ApplyE(t, options) 
 	if err != nil {	log.Println(err) }
 	t.Log(apply)		
-	
 
 	region := "us-west-2"
-	//expectedTagKey1 := "Name"
-	//expectedTagValue1 := "Flugel"
-	//expectedTagKey2 := "Owner"
-	///expectedTagValue2 := "InfraTeam"
 
 	tags := make(map[string]string)
 
@@ -48,15 +43,9 @@ func TestTerraformS3AndEC2Tags(t *testing.T) {
 	instanceID := terraform.Output(t, options, "instance_id")
 	actualInstanceTags := aws.GetTagsForEc2Instance(t, region, instanceID)
 
-	// Verify that our expected name tag is one of the tags
-	//nameTag, containsNameTag := actualBucketTags[tags]
-	//assert.True(t, containsNameTag)
 	assert.Equal(t, tags, actualBucketTags)
 	assert.Equal(t, tags, actualInstanceTags)
 
-	//assert.Equal(t, expectedTagValue1, actualBucketTags[expectedTagKey1]  )
-	//assert.Equal(t, expectedTagValue1, actualInstanceTags[expectedTagKey1])
-	//assert.Equal(t, expectedTagValue2, actualBucketTags[expectedTagKey2]  )
-	//assert.Equal(t, expectedTagValue2, actualInstanceTags[expectedTagKey2])
+	
 
 }
